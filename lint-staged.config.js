600e40a44edc9fbb5e342fab71c3e6d29262af63
Your Name <getbackwards@gmail.com>
2020-08-24 11:02:46 +1200

    deleted lint-staged and --allow-empty

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-const micromatch = require('micromatch');
-
-module.exports = {
-  '*.md': (files) => {
-    // from `files` filter those _NOT_ matching `*test.js`
-    const match = micromatch.not(files, 'README.md');
-    return `eslint ${match.join(' ')}`;
-  },
-};
