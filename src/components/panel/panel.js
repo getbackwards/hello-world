22d9cb6524496ef93e4b491eb275aa7f5aa83b35
Your Name <getbackwards@gmail.com>
2021-02-03 10:24:08 +1300

    added lobotomized owl and material UI button

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 import './panel.scss';
 
 class Panel extends Component {
@@ -6,6 +7,7 @@ class Panel extends Component {
     return (
       <aside className="panel">
         <h2 className="panel__heading">Panel</h2>
+        <Button variant="contained">Hello World</Button>
       </aside>
     );
   }
